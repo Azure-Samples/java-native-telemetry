2c78f88becdaa389469c6e7f1a0113f345b99e51
Sandra Ahlgrimm <sandra.kriemann@gmail.com>
2024-04-12 22:41:27 +0200

    small bugfix

--- code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/repository/VeggieRepository.java
+++ code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/repository/VeggieRepository.java
@@ -3,5 +3,5 @@ package com.azure.examples.springboot.repository;
 import com.azure.examples.springboot.model.Veggie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VeggieRepository extends JpaRepository<Veggie, Long> {
+public interface VeggieRepository extends JpaRepository<Veggie, String> {
 }
