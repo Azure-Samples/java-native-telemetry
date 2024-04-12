08162e6d463395e1ce4de0fef4b23a93f67af58a
SandraAhlgrimm <sakriema@microsoft.com>
2024-04-12 15:48:10 +0200

    merge main

--- code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/service/VeggieService.java
+++ code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/service/VeggieService.java
@@ -3,7 +3,6 @@ package com.azure.examples.springboot.service;
 import com.azure.examples.springboot.data.VeggieItem;
 import com.azure.examples.springboot.model.Veggie;
 import com.azure.examples.springboot.repository.VeggieRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,45 +11,44 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class VeggieService {
-  private final JdbcTemplate jdbcTemplate;
-  private final VeggieRepository veggieRepository;
+    private final VeggieRepository veggieRepository;
 
-  public VeggieService(JdbcTemplate jdbcTemplate, VeggieRepository veggieRepository) {
-    this.jdbcTemplate = jdbcTemplate;
-    this.veggieRepository = veggieRepository;
-  }
+    public VeggieService(VeggieRepository veggieRepository) {
+        this.veggieRepository = veggieRepository;
+    }
 
-  @Transactional
-  public void initializeDatabase() {
-    Veggie carrot = new Veggie("Carrot", "Orange");
-    Veggie broccoli = new Veggie("Broccoli", "Green");
-    Veggie cauliflower = new Veggie("Cauliflower", "White");
+    @Transactional
+    public void initializeDatabase() {
+        Veggie carrot = new Veggie("Carrot", "Orange");
+        Veggie broccoli = new Veggie("Broccoli", "Green");
+        Veggie cauliflower = new Veggie("Cauliflower", "White");
 
-    veggieRepository.save(carrot);
-    veggieRepository.save(broccoli);
-    veggieRepository.save(cauliflower);
-  }
+        veggieRepository.save(carrot);
+        veggieRepository.save(broccoli);
+        veggieRepository.save(cauliflower);
+    }
 
-  @Transactional
-  public VeggieItem addVeggie(VeggieItem veggie) {
-    Veggie veggieEntity = new Veggie(veggie.getName(), veggie.getDescription());
-    Veggie savedVeggie = veggieRepository.save(veggieEntity);
-    return new VeggieItem(savedVeggie.getId(), savedVeggie.getName(), savedVeggie.getDescription());
-  }
+    @Transactional
+    public VeggieItem addVeggie(VeggieItem veggie) {
+        Veggie veggieEntity = new Veggie(veggie.getName(), veggie.getDescription());
+        Veggie savedVeggie = veggieRepository.save(veggieEntity);
+        return new VeggieItem(savedVeggie.getId(), savedVeggie.getName(), savedVeggie.getDescription());
+    }
 
-  @Transactional
-  public void deleteVeggie(Long id) {
-    veggieRepository.deleteById(id);
-  }
+    @Transactional
+    public void deleteVeggie(Long id) {
+        veggieRepository.deleteById(id);
+    }
 
-  public List<VeggieItem> getAllVeggies() {
-    List<Veggie> veggies = veggieRepository.findAll();
-    return veggies.stream().map(veggie -> new VeggieItem(veggie.getId(), veggie.getName(), veggie.getDescription())).toList();
-  }
+    public List<VeggieItem> getAllVeggies() {
+        List<Veggie> veggies = veggieRepository.findAll();
+        return veggies.stream().map(veggie -> new VeggieItem(veggie.getId(), veggie.getName(), veggie.getDescription()))
+                .toList();
+    }
 
-  public VeggieItem getVeggieById(Long id) {
-    return veggieRepository.findById(id)
-      .map(veggie -> new VeggieItem(veggie.getId(), veggie.getName(), veggie.getDescription()))
-      .orElse(null);
-  }
+    public VeggieItem getVeggieById(Long id) {
+        return veggieRepository.findById(id)
+                .map(veggie -> new VeggieItem(veggie.getId(), veggie.getName(), veggie.getDescription()))
+                .orElse(null);
+    }
 }
