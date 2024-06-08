c6a5bdfc75c864b4e0754e8a4df984ddc56d0c9b
Bruno Baptista <brunobat@gmail.com>
2024-06-08 19:39:56 +0100

    New main with all the monitoring on (#52)
    * added otel
    
    * add application insights connection
    
    * Create an auto-deploy file
    
    * change order
    
    * clean dockerfile
    
    * JAR signature verification for ahead-of-time (AOT)
    
    * update container app name
    
    * update paths
    
    * update action
    
    * update java version
    
    * add buildarg
    
    * update graalvm options
    
    * add graalvm native
    
    * change to ce22 instead of muslib
    
    * wrap args
    
    * jvmarg
    
    * update folder structure
    
    * remove optional arg
    
    * update path
    
    * update path
    
    * Adding 'Init-at-build-time' args and removing musl
    
    * Back to using MUSL
    
    * Update Dockerfile to use 21
    
    * Update Dockerfile
    
    * update gh action for main branch
    
    * add spring app name
    
    * Update sakriema-native-spring-AutoDeployTrigger-a70b357f-0a8a-41f1-9ce9-53a5cef3123e.yml
    
    * Create an auto-deploy file
    
    * set trigger path fot whole quarkus superheros app
    
    * Update quarkus-native-monitoring-AutoDeployTrigger-cd1d418e-9e7d-4855-bcfa-dc4948f26827.yml
    
    use absolute path
    
    * Update quarkus-native-monitoring-AutoDeployTrigger-cd1d418e-9e7d-4855-bcfa-dc4948f26827.yml
    
    reset to relative path
    
    * Update quarkus-native-monitoring-AutoDeployTrigger-cd1d418e-9e7d-4855-bcfa-dc4948f26827.yml
    
    debug echos for gh action
    
    * add env variables
    
    * typo
    
    * add path
    
    * Copy multistage dockerfile from src/main/docker
    
    * Update quarkus-native-monitoring-AutoDeployTrigger-cd1d418e-9e7d-4855-bcfa-dc4948f26827.yml
    
    change database url back to pure postkresql
    
    * remove custom sys vars from dockerfile
    
    * cleanups
    
    * DB config tweak
    
    * add application insights connection string
    
    * typo
    
    * add quarkus call
    
    * merge main
    
    * Endpoint with exception
    
    * Update aca-spring-native-AutoDeployTrigger-78625412-9760-4d65-82c5-5b228fd9ff1b.yml
    
    * Unlink the containerApp aca-spring-native from this repo
    
    * remove IT test
    
    * Update sakriema-native-spring-AutoDeployTrigger-a70b357f-0a8a-41f1-9ce9-53a5cef3123e.yml
    
    * remove action to reset it
    
    * remove action from branch
    
    * Create an auto-deploy file
    
    * add env variables
    
    * add missing dependency
    
    * readd needed buildArg to pom.xml
    
    * skip tests
    
    * Update application.properties
    
    * recreate table at service deployment
    
    * add misssing superhero service url
    
    * enable root for spring service
    
    * Update application.properties
    
    Revert to update
    
    * use Strings for ID
    
    * create db
    
    * update orm in quarkus
    
    * Update quarkus-native-monitoring-AutoDeployTrigger-cd1d418e-9e7d-4855-bcfa-dc4948f26827.yml
    
    update database url
    
    * Update Spring app
    
    * hibernate generation mode
    
    * Rename Quarkus app
    
    (cherry picked from commit bc49d3cfa80902dbef548b450311632883d754ff)
    
    * update tests
    
    * Hello  DevoxxFR
    
    * Rename branches to main on github actions
    
    * Update code/spring-boot-telemetry/src/main/resources/application.properties
    
    * Update code/spring-boot-telemetry/src/main/resources/application.properties
    
    * address PR review
    
    ---------
    
    Co-authored-by: SandraAhlgrimm <sakriema@microsoft.com>
    Co-authored-by: Sandra Ahlgrimm <sandra.kriemann@gmail.com>
    Co-authored-by: Dominique Broeglin <dominique.broeglin@gmail.com>
    Co-authored-by: Jean Bisutti <jean.bisutti@gmail.com>

--- code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/controller/VeggieController.java
+++ code/spring-boot-telemetry/src/main/java/com/azure/examples/springboot/controller/VeggieController.java
@@ -16,57 +16,56 @@ import java.util.List;
 @RestController
 @RequestMapping("/veggies")
 public class VeggieController {
+    private final Logger logger = LoggerFactory.getLogger(VeggieController.class);
 
-  private final Logger logger = LoggerFactory.getLogger(VeggieController.class);
+    private final VeggieService veggieService;
+    private final RestTemplate restTemplate;
 
-  private final VeggieService veggieService;
-  private final RestTemplate restTemplate;
+    @Value("${client.superhero.url}")
+    private String restClientUrl;
 
-  @Value("${client.superhero.url}")
-  private String restClientUrl;
-
-  VeggieController(VeggieService veggieService, RestTemplate restTemplate) {
-    this.veggieService = veggieService;
-    this.restTemplate = restTemplate;
-  }
-
-  @PostMapping("/init")
-  public ResponseEntity<String> initializeDatabase() {
-    veggieService.initializeDatabase();
-    return ResponseEntity.ok("Database initialized with 3 veggie items");
-  }
-
-  @PostMapping
-  public ResponseEntity<VeggieItem> addVeggie(@RequestBody VeggieItem veggie) {
-    VeggieItem savedVeggie = veggieService.addVeggie(veggie);
-    try {
-      String url = restClientUrl + "/heroes/veggie";
-      restTemplate.postForEntity(url, savedVeggie.getName(), String.class);
-    } catch (Exception e) {
-      logger.error("Failed to notify SuperHero API", e);
+    VeggieController(VeggieService veggieService, RestTemplate restTemplate) {
+        this.veggieService = veggieService;
+        this.restTemplate = restTemplate;
     }
-    return ResponseEntity.status(HttpStatus.CREATED).body(savedVeggie);
-  }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<String> deleteVeggie(@PathVariable("id") Long id) {
-    veggieService.deleteVeggie(id);
-    return ResponseEntity.ok("Veggie deleted successfully");
-  }
-
-  @GetMapping
-  public ResponseEntity<List<VeggieItem>> getAllVeggies() {
-    List<VeggieItem> veggies = veggieService.getAllVeggies();
-    return ResponseEntity.ok(veggies);
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<VeggieItem> getVeggieById(@PathVariable("id") Long id) {
-    VeggieItem veggie = veggieService.getVeggieById(id);
-    if (veggie != null) {
-      return ResponseEntity.ok(veggie);
-    } else {
-      return ResponseEntity.notFound().build();
+    @PostMapping("/init")
+    public ResponseEntity<String> initializeDatabase() {
+        veggieService.initializeDatabase();
+        return ResponseEntity.ok("Database initialized with 3 veggie items");
     }
-  }
+
+    @PostMapping
+    public ResponseEntity<VeggieItem> addVeggie(@RequestBody VeggieItem veggie) {
+        VeggieItem savedVeggie = veggieService.addVeggie(veggie);
+        try {
+            String url = restClientUrl + "/heroes/veggie";
+            restTemplate.postForEntity(url, savedVeggie.getName(), String.class);
+        } catch (Exception e) {
+            logger.error("Failed to notify SuperHero API", e);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedVeggie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteVeggie(@PathVariable("id") Long id) {
+        veggieService.deleteVeggie(id);
+        return ResponseEntity.ok("Veggie deleted successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VeggieItem>> getAllVeggies() {
+        List<VeggieItem> veggies = veggieService.getAllVeggies();
+        return ResponseEntity.ok(veggies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VeggieItem> getVeggieById(@PathVariable("id") Long id) {
+        VeggieItem veggie = veggieService.getVeggieById(id);
+        if (veggie != null) {
+            return ResponseEntity.ok(veggie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
